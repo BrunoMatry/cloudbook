1199366aea45de622ad609384795773c54216cdd
gwandalf <gwlemoul@insa-rennes.fr>
2014-02-19 18:42:46 +0100

    package nodeview

---
+++ HMI-JavaFX/src/model/friendmanager/CloudBookNode.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model.friendmanager;
+
+/**
+ *
+ * @author Gwendal
+ * 
+ * Representation of a node
+ */
+public class CloudBookNode {
+    
+}
