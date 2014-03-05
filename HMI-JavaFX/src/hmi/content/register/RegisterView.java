8c924102b3b1540d2ee60e04015bcf6c137ed127
gwandalf <gwlemoul@insa-rennes.fr>
2014-03-05 18:44:02 +0100

    change of logo ok

--- HMI-JavaFX/src/hmi/content/register/RegisterView.java
+++ HMI-JavaFX/src/hmi/content/register/RegisterView.java
@@ -6,17 +6,31 @@
 
 package hmi.content.register;
 
+import hmi.Launcher;
 import hmi.button.IconFlyWeight;
 import hmi.content.AActivity;
 import hmi.content.Activity;
 import hmi.home.HomeView;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 
 /**
  * View letting the user register an application
@@ -44,6 +58,7 @@ public final class RegisterView extends Activity<BorderPane> {
     public class MyVBox extends VBox {
         
         private ImageView logo;
+        private Button browse;
         private TextField name;
         private ChoiceBox clouds;
         private TableView mesures;
@@ -51,10 +66,76 @@ public final class RegisterView extends Activity<BorderPane> {
         public MyVBox() {
             super();
             setAlignment(Pos.CENTER);
-            logo = new ImageView(IconFlyWeight.INSTANCE.getDefaultLogo());
-            name = new TextField("App name");
-            clouds = new ChoiceBox();
-            getChildren().addAll(logo, name, clouds);
+            setSpacing(50);
+            getChildren().addAll(
+                    getLogo(),
+                    getBrowse(),
+                    getName(),
+                    getClouds()
+            );
         }
+        
+        public final ImageView getLogo() {
+            if(logo == null) {
+                logo = new ImageView(IconFlyWeight.INSTANCE.getDefaultLogo());
+            }
+            return logo;
+        }
+
+        public void setLogo(ImageView logo) {
+            if(this.logo != null)
+                getChildren().remove(this.logo);
+            this.logo = logo;
+            getChildren().add(0, this.logo);
+        }
+
+        public final ChoiceBox getClouds() {
+            if(clouds == null) {
+                clouds = new ChoiceBox();
+            }
+            return clouds;
+        }
+
+        public final Button getBrowse() {
+            if(browse == null) {
+                browse = new Button("Browse...");
+                browse.setOnAction(new EventHandler<ActionEvent>() {
+
+                    @Override
+                public void handle(ActionEvent t) {
+                    FileInputStream fis = null;
+                        try {
+                            FileChooser files = new FileChooser();
+                            File f = files.showOpenDialog(Launcher.STAGE);
+                            fis = new FileInputStream(f);
+                            Image i = new Image(fis);
+                            ImageView iv = new ImageView(i);
+                            MyVBox parent = (MyVBox)browse.getParent();
+                            parent.setLogo(iv);
+                            
+                        } catch (FileNotFoundException ex) {
+                            Logger.getLogger(RegisterView.class.getName()).log(Level.SEVERE, null, ex);
+                        } finally {
+                            try {
+                                fis.close();
+                            } catch (IOException ex) {
+                                Logger.getLogger(RegisterView.class.getName()).log(Level.SEVERE, null, ex);
+                            }
+                        }
+                }
+                    
+                });
+            }
+            return browse;
+        }
+
+        public final TextField getName() {
+            if(name == null) {
+                name = new TextField("App name");
+                setMargin(name, new Insets(0, 200, 0, 200));
+            }
+            return name;
+        }
+        
     }
  }
