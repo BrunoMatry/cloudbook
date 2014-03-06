f201575a5f5bc03b5582c42df7ee751eafc8aad6
BrunoMatry <brunomatry@gmail.com>
2014-03-06 02:28:06 +0100

    Class Friend

--- HMI-JavaFX/src/modele/node/Cloud.java
+++ HMI-JavaFX/src/modele/node/Cloud.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package modele.node;
 
 /**
- *
- * @author Bruno
+ * Enum Cloud
+ * 
+ * Note : cet enum sera bien evidement amene a etre complete 
+ * voir a etre remplace par une autre forme de structure de donnees.
  */
 public enum Cloud {
-    
+    GDRIVE, SKYDRIVE, DROPBOX
 }
--- HMI-JavaFX/src/modele/node/Friend.java
+++ HMI-JavaFX/src/modele/node/Friend.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package modele.node;
 
 import java.util.Date;
@@ -12,11 +6,37 @@ import java.util.Date;
  *
  * @author Bruno
  */
-public class Friend implements Information {
-    private int confidence;
+public class Friend implements Information {  
     private int id;
-    private Date lastConnexion;
+    private int confidence;
     private boolean relevant;
     private AppVector vector;
+    private Date lastConnexion;
     
+    public Friend(int id, int confidence, boolean relevant, AppVector vector) {
+        this.id = id;
+        this.confidence = confidence;
+        this.relevant = relevant;
+        this.vector = vector;
+        this.connexion();
+    }
+    
+    public void setConfidence(int confidence) {
+        this.confidence = confidence;
+        this.connexion();
+    }
+    
+    public void setRelevance(boolean relevant) {
+        this.relevant = relevant;
+        this.connexion();
+    }
+    
+    public void setVector(AppVector vector) {
+        this.vector = vector;
+        this.connexion();
+    }
+            
+    private void connexion() {
+        this.lastConnexion = new Date();
+    }
 }
