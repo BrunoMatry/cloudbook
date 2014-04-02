85b5f1e5efb172ee9b21cb86883a5c77d434189e
BrunoMatry <brunomatry@gmail.com>
2014-04-02 13:16:58 +0200

    Clean project files

--- HMI-JavaFX/src/model/friendmanager/Information.java
+++ HMI-JavaFX/src/model/friendmanager/Information.java
@@ -13,6 +13,6 @@ import java.io.Serializable;
  * @author Bruno
  */
 public interface Information extends Serializable {
-    public void saveProperties();
-    public void restoreProperties();
+    void saveProperties();
+    void restoreProperties();
 }
