6c1d6ec8ace9162c2c8538d40e6482978483aca9
BrunoMatry <brunomatry@gmail.com>
2014-04-15 10:55:46 +0200

    Message Test

---
+++ HMI-JavaFX/test/test/modele/node/MessageTest.java
@@ -0,0 +1,36 @@
+package test.modele.node;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import static org.junit.Assert.assertTrue;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class MessageTest {
+    
+    public MessageTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testConstructeur() {
+        /* TODO */
+    }
+
+}
