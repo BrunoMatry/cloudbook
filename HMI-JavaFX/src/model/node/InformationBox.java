7ab9b0e8cac6316c6b401bce7421c8112a923a01
gwandalf <gwlemoul@insa-rennes.fr>
2014-04-23 16:32:40 +0200

    information box test ok

--- HMI-JavaFX/src/model/node/InformationBox.java
+++ HMI-JavaFX/src/model/node/InformationBox.java
@@ -6,7 +6,9 @@
 
 package model.node;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Stack;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import model.network.interfaces.Information;
 
@@ -16,11 +18,89 @@ import model.network.interfaces.Information;
  * box containing some informations with string property describing the box content
  * @param <T> type of information
  */
-public class InformationBox<T extends Information> {
+public class InformationBox<T extends Information> implements Information {
     
-    protected List<T> box; //box containing information
+    public static final String EMPTY_FLAG = "No information to display at this time";
+    
+    protected Stack<T> box; //box containing information
     protected String _description; //description of the box
-    protected StringProperty description; //description property
     
+    protected transient StringProperty description; //description property
+    public StringProperty descriptionProperty() {
+        return description;
+    }
+
+    /**
+     * Constructor
+     */
+    public InformationBox() {
+        box = new Stack<>();
+        description = new SimpleStringProperty(EMPTY_FLAG);
+    }
+    
+    /**
+     * Adds information to the box and update the description
+     * @param info information to add
+     */
+    public void push(T info) {
+        box.push(info);
+        description.set(info.toString());
+    }
+    
+    /**
+     * Pops information from the stack
+     * @return top information
+     */
+    public T pop() {
+        T info = box.pop();
+        if(box.empty())
+            description.set(EMPTY_FLAG);
+        else
+            description.set(box.peek().toString());
+        return info;
+    }
+    
+    /**
+     * Removes specified information from the box
+     * Description is set to the new peek if needed
+     * @param info information to remove
+     */
+    public void remove(T info) {
+        if(info == box.peek())
+            this.pop();
+        else {
+            box.remove(info);
+            if(box.isEmpty())
+                description.set(EMPTY_FLAG);
+        }
+    }
+    
+    /**
+     * Empty predicate
+     * @return true if the box doesn't contains elements, false otherwise
+     */
+    public boolean empty() {
+        return box.empty();
+    }
+    
+    /**
+     * Saves all properties of contained informations
+     */
+    @Override
+    public void saveProperties() {
+        for(Information info : box)
+            info.saveProperties();
+        _description = description.get();
+    }
+
+    /**
+     * Restores all properties of contained informations
+     */
+    @Override
+    public void restoreProperties() {
+        for(Information info : box)
+            info.restoreProperties();
+        description = new SimpleStringProperty(_description);
+    }
     
 }
---
+++ HMI-JavaFX/test/test/modele/node/InformationBoxTest.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package test.modele.node;
+
+import junit.framework.Assert;
+import model.network.interfaces.Information;
+import model.node.InformationBox;
+import model.node.Message;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author Gwendal
+ */
+public class InformationBoxTest {
+    
+    public InformationBoxTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @Test
+    public void PushTest() {
+        InformationBox<Information> infoBox = new InformationBox<>();
+        Assert.assertTrue(infoBox.empty());
+        Assert.assertEquals(InformationBox.EMPTY_FLAG, infoBox.descriptionProperty().get());
+        infoBox.push(new TestInformation("Alice"));
+        Assert.assertFalse(infoBox.empty());
+        Assert.assertEquals("Alice", infoBox.descriptionProperty().get());
+        infoBox.push(new TestInformation("Bob"));
+        Assert.assertFalse(infoBox.empty());
+        Assert.assertEquals("Bob", infoBox.descriptionProperty().get());
+    }
+    
+    @Test
+    public void PopTest() {
+        InformationBox<Information> infoBox = new InformationBox<>();
+        Information alice = new TestInformation("Alice");
+        Information bob = new TestInformation("Bob");
+        infoBox.push(alice);
+        infoBox.push(bob);
+        Information first = infoBox.pop();
+        Assert.assertEquals(bob, first);
+        Assert.assertEquals("Alice", infoBox.descriptionProperty().get());
+        Information second = infoBox.pop();
+        Assert.assertEquals(alice, second);
+        Assert.assertEquals(InformationBox.EMPTY_FLAG, infoBox.descriptionProperty().get());
+        Assert.assertTrue(infoBox.empty());
+    }
+    
+    @Test
+    public void RemoveTest() {
+        InformationBox<Information> infoBox = new InformationBox<>();
+        Information alice = new TestInformation("Alice");
+        Information bob = new TestInformation("Bob");
+        infoBox.push(alice);
+        infoBox.push(bob);
+        infoBox.remove(alice);
+        Assert.assertEquals("Bob", infoBox.descriptionProperty().get());
+        infoBox.remove(bob);
+        Assert.assertEquals(InformationBox.EMPTY_FLAG, infoBox.descriptionProperty().get());
+        Assert.assertTrue(infoBox.empty());
+    }
+    
+    private static final class TestInformation implements Information {
+
+        private String label;
+        
+        public TestInformation(String label) {
+            this.label = label;
+        }
+
+        @Override
+        public String toString() {
+            return label;
+        }
+        
+        @Override
+        public void saveProperties() {
+        }
+
+        @Override
+        public void restoreProperties() {
+        }
+        
+    }
+}
