59bb176b1aca16911a8d45210429be347a9b1cf1
Bruno Matry <brunomatry@gmail.com>
2014-05-19 15:30:09 +0200

    HMI

--- HMI-JavaFX/src/hmi/content/result/ContentResultTab.java
+++ HMI-JavaFX/src/hmi/content/result/ContentResultTab.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package hmi.content.result;
 
 import hmi.content.Content;
 
-/**
- *
- * @author Bruno
- */
 public class ContentResultTab extends Content {
     private SuggestionView suggestion;
     private ResultBoardView board;
