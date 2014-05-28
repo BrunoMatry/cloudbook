4305b19930faea32a955ef3baff76f9b8185117d
gwandalf <gwlemoul@insa-rennes.fr>
2014-05-28 11:37:20 +0200

    AppVectorEditer doc

--- CloudBookClient/src/hmi/content/node/AppVectorEditer.java
+++ CloudBookClient/src/hmi/content/node/AppVectorEditer.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package hmi.content.node;
 
 import hmi.Launcher;
@@ -21,7 +15,7 @@ import model.node.FileEngineRelation;
 import model.node.MyNode;
 
 /**
- *
+ * Graphic chart which allow to modify a vector of features.
  * @author Gwendal
  */
 public class AppVectorEditer extends HBox {
@@ -32,18 +26,36 @@ public class AppVectorEditer extends HBox {
     private Button apply;
     private AppVectorBuilder builder;
     
+    /**
+     * Type of the application.
+     * @return Type of the application.
+     */
     public StringProperty typeProperty() {
         return type.getHint().textProperty();
     }
     
+    /**
+     * Performance need of the application.
+     * @return Performance need of the application.
+     */
     public StringProperty perfProperty() {
         return perf.getHint().textProperty();
     }
     
+    /**
+     * Speed need of the application.
+     * @return Speed need of the application.
+     */
     public StringProperty speedProperty() {
         return speed.getHint().textProperty();
     }
     
+    /**
+     * Constructor
+     * @param type application type
+     * @param perf performance need
+     * @param speed speed need 
+     */
     public AppVectorEditer(String type, String perf, String speed) {
         this.type = new Field("Application type : ", type);
         this.perf = new Field("Performance need : ", perf);
@@ -52,6 +64,9 @@ public class AppVectorEditer extends HBox {
         getChildren().addAll(this.type, this.perf, this.speed, this.apply);
     }
     
+    /**
+     * Sets up the validation button (title and action).
+     */
     private void setUpApply() {
         apply = new Button("Apply");
         apply.setOnAction(new EventHandler<ActionEvent>() {
@@ -67,6 +82,10 @@ public class AppVectorEditer extends HBox {
         });
     }
 
+    /**
+     * Setter
+     * @param builder of the vector.
+     */
     public void setBuilder(AppVectorBuilder builder) {
         this.builder = builder;
     }
