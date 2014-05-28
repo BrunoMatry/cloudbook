fcd27f0631e4be4b8b394e1c501699193bd0cc76
gwandalf <gwlemoul@insa-rennes.fr>
2014-05-28 14:30:33 +0200

    Cloud doc

--- CloudBookClient/src/model/node/Cloud.java
+++ CloudBookClient/src/model/node/Cloud.java
@@ -3,9 +3,7 @@ package model.node;
 import java.io.Serializable;
 
 /**
- * Enum Cloud
- * Note : cet enum sera bien evidement amene a etre complete 
- * voir a etre remplace par une autre forme de structure de donnees.
+ * Enum Clouds platforms
  */
 public enum Cloud implements Serializable {
     GOOGLE,
