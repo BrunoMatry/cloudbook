99448f306630ac8f6e9aa2ea66a32b8af43df7f3
gwandalf <gwlemoul@insa-rennes.fr>
2014-05-28 09:40:50 +0200

    observbale property doc

--- CloudBookClient/src/controller/ObservableProperty.java
+++ CloudBookClient/src/controller/ObservableProperty.java
@@ -1,17 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package controller;
 
 import java.util.Observer;
 
 /**
- *
+ * Observer
+ * A Javafx property which is also observable by observers.
  * @author Gwendal
  */
 public interface ObservableProperty {
+    
+    /**
+     * Adds an observer to this observable property.
+     * @param obs observer to be added.
+     */
     void addObserver(Observer obs);
 }
