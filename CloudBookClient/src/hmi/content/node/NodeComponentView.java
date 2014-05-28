0d4eeee9e8f5acca55a92b68893c55d8b03b99db
gwandalf <gwlemoul@insa-rennes.fr>
2014-05-28 11:42:19 +0200

    NodeComponentView doc

--- CloudBookClient/src/hmi/content/node/NodeComponentView.java
+++ CloudBookClient/src/hmi/content/node/NodeComponentView.java
@@ -5,6 +5,11 @@ import hmi.content.OneNodeActivity;
 import javafx.scene.control.TableView;
 import model.network.interfaces.Information;
 
+/**
+ * Graphic paned containing a table view in order to display a list of objects of a specified type.
+ * @author Gwendal
+ * @param <I> Type of information wich is to be displayed.
+ */
 public abstract class NodeComponentView<I extends Information> extends OneNodeActivity implements IComponentView {
     
     //Table displaying some information about the current application node 
