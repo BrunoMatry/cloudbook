20d6cad9f41e608df03e7d3936b5981bd1b8f064
gwandalf <gwlemoul@insa-rennes.fr>
2014-05-28 15:16:44 +0200

    RemoteOperations doc

--- RemoteLibrary/RemoteLibrary/src/model/network/interfaces/RemoteOperations.java
+++ RemoteLibrary/RemoteLibrary/src/model/network/interfaces/RemoteOperations.java
@@ -10,11 +10,30 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 /**
- *
+ * Network operations shared by clients and servers.
  * @author Gwendal
  */
 public interface RemoteOperations extends Remote {
+    
+    /**
+     * Sends a request to a receiver.
+     * @param request request to be sent.
+     * @param receiver receiver of the request.
+     * @throws RemoteException problem with remote connection.
+     */
     public void send(Sendable request, String receiver) throws RemoteException;
+    
+    /**
+     * Sends a request to all the clients on the network.
+     * @param request request to be sent.
+     * @throws RemoteException remote connection problem.
+     */
     public void broadcast(Sendable request) throws RemoteException;
+    
+    /**
+     * Gets the identifier of the client/server on the network.
+     * @return the identifier of the client/server on the network.
+     * @throws RemoteException remote connection problem.
+     */
     public String getId() throws RemoteException;
 }
