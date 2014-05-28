0ab2e51a188d63bffdd60f4f88014f4716c9c7ff
gwandalf <gwlemoul@insa-rennes.fr>
2014-05-28 16:11:00 +0200

    MyNode doc

--- CloudBookClient/src/model/node/MyNode.java
+++ CloudBookClient/src/model/node/MyNode.java
@@ -103,32 +103,123 @@ public class MyNode implements Serializable {
             this.mesures.push(m);
     }
     
-    public void addMessage(Message m) { messages.push(m); } 
+    /**
+     * Adds a message to the node.
+     * @param m message to be added.
+     */
+    public void addMessage(Message m) { messages.push(m); }
+    
+    /**
+     * Adds an information to the node.
+     * @param info information to be added.
+     */
     public void addInformation(Information info) { informations.add(info); }
+    
+    /**
+     * Adds a friend to the node.
+     * @param f friend to be added.
+     */
     public void addFriend(Friend f) { friends.push(f); }
     
+    /**
+     * Getter
+     * @return the friend set.
+     */
     public InformationBox getFriends() { return friends; }
+    
+    /**
+     * Getter
+     * @return the vector of features.
+     */
     public AppVector getVector() { return vector.get(); }
+    
+    /**
+     * Getter
+     * @return the information list.
+     */
     public List<Information> getInformations() { return informations; }
+    
+    /**
+     * Getter
+     * @param i index of the message.
+     * @return searched message.
+     */
     public Message getMessage(int i) { return messages.get(i); }
+    
+    /**
+     * Getter
+     * @return the message set.
+     */
     public InformationBox getMessages() { return messages; }
+    
+    /**
+     * Getter
+     * @return the mesure set.
+     */
     public InformationBox getMesures() { return mesures; }
+    
+    /**
+     * Getter
+     * @return the application history.
+     */
     public InformationBox getStates() { return states; }
     
+    /**
+     * Getter
+     * @return the port used by the client to communicate on the network.
+     */
     public int getNodePort() { return nodePort; }
+    
+    /**
+     * Getter
+     * @return the current platform of the application.
+     */
     public final Cloud getPlatform() { return this.platform.get(); }
-      
+    
+    /**
+     * Representative mesure of the mesure set.
+     * @return Representative mesure of the mesure set.
+     */
     public StringProperty topMesureProperty() { return topMesure.dateProperty(); }
+    
+    /**
+     * Application name.
+     * @return Application name.
+     */
     public StringProperty nameProperty() { return name; }
+    
+    /**
+     * Application logo.
+     * @return Application logo.
+     */
     public StringProperty logoProperty() { return logo; }
+    
+    /**
+     * Current cloud platform of the application.
+     * @return Current cloud platform of the application.
+     */
     public CloudProperty platformProperty() { return platform; }
+    
+    /**
+     * Application features vector.
+     * @return Application feautures vector.
+     */
     public ObjectProperty<AppVector> vectorProperty() { return vector; }
     
+    /**
+     * Setter
+     * @param informations information list.
+     */
     public void setInformations(List<Information> informations) { this.informations = informations; }
+    
+    /**
+     * Setter
+     * @param vector application features vector.
+     */
     public void setVector(AppVector vector) { this.vector.set(vector); }
       
     /**
-     * updates the cloud-platform of the current application
+     * Updates the cloud-platform of the current application
      * @param c new cloud-platform
      */
     public void majCurrentState(Cloud c) {
@@ -137,7 +228,6 @@ public class MyNode implements Serializable {
         if(!c.equals(currentState.getCloud())) {
             currentState.notCurrentAnymore();
             states.push(new State(c));
-            System.out.println("Appel du setter de MyNode::platform");
             platform.set(c);
         }    
     }
